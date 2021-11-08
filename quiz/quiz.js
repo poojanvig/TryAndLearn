21b2828bda8a88197268cc9d3a94a4ef89c58974
Poojan <poojanvig.pv@gmail.com>
2021-11-08 18:38:24 +0530

    added quiz folder

---
+++ quiz/quiz.js
@@ -0,0 +1,26 @@
+
+const quize = [
+    {
+        q:'Guess the output int main() { int a = 5; int b = 10; { int a = 2; a++; b++; } printf("%d %d", a, b); return 0; }',
+        options: ['6,11','11,6','6,10','5,11'],
+        answer:3
+    },
+    {
+        q:'Which of the following is not a logical opertaor ',
+        options: ['||',' !=','&&','&='],
+        answer:3
+    },
+    {
+        q:'What is output of below program?int main(){int i;for(i=0; i<5; i++);{printf("cppbuzz");}return 0;} ',
+        options: ['cppbuzz is printed 5 times',' Compilation Error','cppbuzz is printed 1 times','Nothing is printed'],
+        answer:2
+    },
+    {
+        q:'Which of the following is not a valid C',
+        options: ['int number;','float rate;','int variable_count','int$main'],
+        answer:3
+    },
+    {
+        
+    }
+]
